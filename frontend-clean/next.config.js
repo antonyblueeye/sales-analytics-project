f3d29b5bf38b3210afbd06985dc12789df266433
Anton <antonyblueeye@gmail.com>
2026-05-25 19:54:51 +0300

    Решена проблема с eval

--- frontend-clean/next.config.js
+++ frontend-clean/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['react-date-range'],
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: [
+                            "default-src 'self'",
+                            "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+                            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+                            "font-src 'self' https://fonts.gstatic.com",
+                            "img-src 'self' data: https: blob:",
+                            "connect-src 'self' https: wss:",
+                        ].join('; '),
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
